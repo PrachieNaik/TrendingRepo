8b12998b3cb21a14f366d22c59ef938c3c213039
Prachi-Naik <prachi@dunzo.in>
2021-06-03 00:09:09 +0530

    Custom adapter setup

--- app/src/main/java/com/example/trendingrepo/TrendingRepoListingAdapter.kt
+++ app/src/main/java/com/example/trendingrepo/TrendingRepoListingAdapter.kt
@@ -1,25 +1,35 @@
 package com.example.trendingrepo
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.trendingrepo.databinding.ItemTrendingRepoBinding
 
 class TrendingRepoListingAdapter : RecyclerView.Adapter<TrendingRepoListingAdapter.TrendingRepoVH>() {
+
+    private val trendingRepoList = mutableListOf<Repo>()
+
+    fun updateData(list: List<Repo>) {
+        trendingRepoList.addAll(list)
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendingRepoVH {
-        TODO("Not yet implemented")
+        val binding = ItemTrendingRepoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrendingRepoVH(binding)
     }
 
     override fun onBindViewHolder(holder: TrendingRepoVH, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(trendingRepoList[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = trendingRepoList.size
 
-    class TrendingRepoVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
-
+    inner class TrendingRepoVH(private val binding: ItemTrendingRepoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(repo: Repo) {
+            binding.name.text = repo.fullName
+            binding.description.text = repo.description
+            binding.language.text = repo.language
         }
     }
 }
